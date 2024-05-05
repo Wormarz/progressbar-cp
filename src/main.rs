d9f046765a1b62d611e8335e657ac2ae99ee0f8b
wormarz <wormarz@outlook.com>
2024-05-05 16:34:49 +0800

    implement basic function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs;
+use std::io::Read;
+use std::io::Write;
+
+fn show_usage() {
+    println!("rs_cp - copy files\nUsage: cp <src> <des>\n");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        show_usage();
+        return;
+    }
+
+    let src_path = &args[1];
+    let des_path = &args[2];
+
+    let mut src_file = fs::File::open(src_path).unwrap();
+    let mut des_file = fs::File::create(des_path).unwrap();
+    let mut buffer = [0u8; 4096];
+
+    loop {
+        let nbytes = src_file.read(&mut buffer).unwrap();
+        des_file.write(&buffer[..nbytes]).unwrap();
+        if nbytes < buffer.len() { break; }
+    }
 }
