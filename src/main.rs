a0482278aaad1ffa82d93770f63e1135414654df
wormarz <wormarz@outlook.com>
2024-05-05 18:12:32 +0800

    restructuring, and support progress bar.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,50 @@
 use std::env;
 use std::fs;
-use std::io::Read;
-use std::io::Write;
-
-fn show_usage() {
-    println!("rs_cp - copy files\nUsage: cp <src> <des>\n");
-}
+use std::process;
+use std::io::{Read, Write};
+use indicatif::ProgressBar;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let cp_configs = Config::new(&args);
 
-    if args.len() < 3 {
-        show_usage();
-        return;
+    do_copy(cp_configs);
+}
+
+fn show_usage() {
+    println!("rs_cp - copy files\nUsage: cp <src> <des>");
+}
+
+struct Config {
+    src: String,
+    des: String,
+}
+
+impl Config {
+    /*constructor */
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            show_usage();
+            process::exit(1);
+        }
+        let src = args[1].clone();
+        let des = args[2].clone();
+
+        Config { src, des }
     }
+}
 
-    let src_path = &args[1];
-    let des_path = &args[2];
-
-    let mut src_file = fs::File::open(src_path).unwrap();
-    let mut des_file = fs::File::create(des_path).unwrap();
+fn do_copy(config: Config) {
+    let mut src_file = fs::File::open(config.src).unwrap();
+    let mut des_file = fs::File::create(config.des).unwrap();
     let mut buffer = [0u8; 4096];
+    let count = src_file.metadata().unwrap().len() as u64;
+    let pb = ProgressBar::new(count);
 
     loop {
         let nbytes = src_file.read(&mut buffer).unwrap();
         des_file.write(&buffer[..nbytes]).unwrap();
+        pb.inc(nbytes.try_into().unwrap());
         if nbytes < buffer.len() { break; }
     }
 }
