1ef31eac78e8e43088eb9f90ffa33231310881ad
wormarz <wormarz@outlook.com>
2024-12-30 23:46:55 +0800

    Update dependencies and refactor file copy logic with Clap integration

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,49 @@
-use std::env;
 use std::fs;
+use clap::Parser;
 use std::process;
 use std::io::{Read, Write};
+use std::process::exit;
 use indicatif::ProgressBar;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let cp_configs = Config::new(&args);
-
-    do_copy(cp_configs);
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = "rs_cp - copy files")]
+struct Args {
+    /// COPY FROM SRCS... TO DES
+    srcs_des: Vec<String>,
 }
 
-fn show_usage() {
-    println!("rs_cp - copy files\nUsage: cp <src> <des>");
-}
-
-struct Config {
-    src: String,
-    des: String,
-}
-
-impl Config {
-    /*constructor */
-    fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            show_usage();
-            process::exit(1);
+impl Args {
+    fn check(&self) {
+        if self.srcs_des.len() < 2 {
+            panic!("Need at least a src and a des");
         }
-        let src = args[1].clone();
-        let des = args[2].clone();
-
-        Config { src, des }
     }
 }
 
-fn do_copy(config: Config) {
-    let mut src_file = fs::File::open(config.src).unwrap();
-    let mut des_file = fs::File::create(config.des).unwrap();
-    let mut buffer = [0u8; 4096];
-    let count = src_file.metadata().unwrap().len() as u64;
-    let pb = ProgressBar::new(count);
+fn main() {
+    let args = Args::parse();
 
-    loop {
-        let nbytes = src_file.read(&mut buffer).unwrap();
-        des_file.write(&buffer[..nbytes]).unwrap();
-        pb.inc(nbytes.try_into().unwrap());
-        if nbytes < buffer.len() { break; }
+    println!("{:?}", args);
+    args.check();
+
+    do_copy(&args);
+}
+
+fn do_copy(args: &Args) {
+    let mut buffer = vec![0u8; 4096];
+    let total_pbar = ProgressBar::new(args.srcs_des.len() as u64 - 1);
+
+    for src in &args.srcs_des[0..args.srcs_des.len() - 1] {
+        let src_file = fs::File::open(src).unwrap();
+        let mut des_file = fs::File::create(args.srcs_des[args.srcs_des.len() - 1].clone() + "/" + src).unwrap();
+        println!("Copying {} to {}", src, args.srcs_des[args.srcs_des.len() - 1].clone() + "/" + src);
+
     }
+    // loop {
+    //     let nbytes = src_file.read(&mut buffer).unwrap();
+    //     des_file.write(&buffer[..nbytes]).unwrap();
+    //     pb.inc(nbytes.try_into().unwrap());
+    //     if nbytes < buffer.len() { break; }
+    // }
 }
