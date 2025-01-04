8e0c1bb0560d95b7034c936d069344585e7d132f
wormarz <wormarz@outlook.com>
2025-01-04 10:42:48 +0800

    Refactor Copier module to include progress tracking and logging functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use clap::Parser;
-use copier::Copier;
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
-use log::{debug, trace};
-use std::fs;
+use log::trace;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -35,46 +32,5 @@ fn main() -> Result<(), std::io::Error> {
     trace!("{:?}", args);
     args.check()?;
 
-    do_copy(&args)
-}
-
-fn do_copy(args: &Args) -> Result<(), std::io::Error> {
-    let mut copier = Copier::new(4096);
-
-    let m = MultiProgress::new();
-    let sty = ProgressStyle::with_template(
-        "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
-    )
-    .unwrap()
-    .progress_chars("##-");
-
-    let total_pbar = m.add(ProgressBar::new(args.srcs_des.len() as u64 - 1));
-    total_pbar.set_style(sty.clone());
-
-    let pb = m.add(ProgressBar::new(0));
-    pb.set_style(sty);
-
-    for src in &args.srcs_des[0..args.srcs_des.len() - 1] {
-        let src_file = fs::File::open(src)?;
-        let des_file =
-            fs::File::create(args.srcs_des[args.srcs_des.len() - 1].clone() + "/" + src)?;
-        let len = src_file.metadata().unwrap().len();
-        pb.set_length(len);
-
-        debug!(
-            "Copying {} to {}",
-            src,
-            args.srcs_des[args.srcs_des.len() - 1].clone() + "/" + src
-        );
-
-        copier.copy(src_file, des_file, None, |copied: u64, _: u64| {
-            pb.set_position(copied);
-            pb.set_message(format!("bytes copied"));
-        })?;
-
-        total_pbar.inc(1);
-        total_pbar.set_message(format!("files copied"));
-    }
-    total_pbar.finish_with_message("All files copied");
-    m.clear()
+    copier::do_copy(&args.srcs_des)
 }
--- utils/copier/src/lib.rs
+++ utils/copier/src/lib.rs
@@ -1,4 +1,7 @@
 use std::io::{Read, Write};
+use log::debug;
+use std::fs;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 
 pub struct Copier {
     buffer: Vec<u8>,
@@ -37,6 +40,47 @@ impl Copier {
     }
 }
 
+
+pub fn do_copy(args: &Vec<String>) -> Result<(), std::io::Error> {
+    let mut copier = Copier::new(4096);
+
+    let m = MultiProgress::new();
+    let sty = ProgressStyle::with_template(
+        "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+    )
+    .unwrap()
+    .progress_chars("##-");
+
+    let total_pbar = m.add(ProgressBar::new(args.len() as u64 - 1));
+    total_pbar.set_style(sty.clone());
+
+    let pb = m.add(ProgressBar::new(0));
+    pb.set_style(sty);
+
+    for src in &args[0..args.len() - 1] {
+        let src_file = fs::File::open(src)?;
+        let des_file =
+            fs::File::create(args[args.len() - 1].clone() + "/" + src)?;
+        let len = src_file.metadata().unwrap().len();
+        pb.set_length(len);
+
+        debug!(
+            "Copying {} to {}",
+            src,
+            args[args.len() - 1].clone() + "/" + src
+        );
+
+        copier.copy(src_file, des_file, None, |copied: u64, _: u64| {
+            pb.set_position(copied);
+            pb.set_message(format!("bytes copied"));
+        })?;
+
+        total_pbar.inc(1);
+        total_pbar.set_message(format!("files copied"));
+    }
+    total_pbar.finish_with_message("All files copied");
+    m.clear()
+}
 
 // #[cfg(test)]
 // mod tests {
