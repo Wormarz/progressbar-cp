8e386c4e15e31d4d4946bbd63fd261075fb19635
wormarz <wormarz@outlook.com>
2025-01-04 22:46:56 +0800

    Add benchmarking for Copier module using Criterion

--- utils/copier/benches/cp_benchmark.rs
+++ utils/copier/benches/cp_benchmark.rs
@@ -1,9 +1,14 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
+use std::fs;
 use copier::Copier;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    c.bench_function("Cpoyier bench", |b| b.iter(|| {
+        let mut copier = Copier::new(black_box(4096));
+        let src_file = fs::File::open("../../testfiles/test1.bin").unwrap();
+        let des_file = fs::File::create("../../testfiles/des/test1.bin").unwrap();
+        copier.copy(src_file, des_file, None, |_, _| {}).unwrap();
+    }));
 }
 
 criterion_group!(benches, criterion_benchmark);
