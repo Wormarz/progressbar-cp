c5e3948dae244a4976438d842514f22be03ad455
wormarz <wormarz@outlook.com>
2025-01-21 22:33:28 +0800

    Enhance scan_dir tests with symlink handling and improve file path usage

--- utils/scanner/src/lib.rs
+++ utils/scanner/src/lib.rs
@@ -31,19 +31,36 @@ mod tests {
     fn scan_dir_works() {
         let temp_dir = tempfile::tempdir_in(".").unwrap();
         let file_path = temp_dir.path().join("my-temporary-note.txt");
-        let _file = File::create(file_path).unwrap();
+        let _file = File::create(&file_path).unwrap();
         let ret = scan_dir(&vec![temp_dir.path().to_str().unwrap().to_string()]).unwrap();
         println!("{:?}", ret);
         assert_eq!(
             ret,
             vec![
                 temp_dir.path().to_str().unwrap().to_string(),
-                temp_dir
-                    .path()
-                    .join("my-temporary-note.txt")
-                    .to_str()
-                    .unwrap()
-                    .to_string()
+                file_path.to_str().unwrap().to_string()
+            ]
+        );
+    }
+
+    #[test]
+    fn scan_stop_at_link() {
+        let temp_dir1 = tempfile::tempdir_in(".").unwrap();
+        let temp_dir2 = tempfile::tempdir_in(".").unwrap();
+
+        let file_path = temp_dir1.path().join("my-temporary-note.txt");
+        let _file = File::create(&file_path).unwrap();
+
+        let link_path = temp_dir2.path().join("my-temporary-note.link");
+        let _link = std::os::unix::fs::symlink(temp_dir1.path(), &link_path).unwrap();
+
+        let ret = scan_dir(&vec![temp_dir2.path().to_str().unwrap().to_string()]).unwrap();
+        println!("{:?}", ret);
+        assert_eq!(
+            ret,
+            vec![
+                temp_dir2.path().to_str().unwrap().to_string(),
+                link_path.to_str().unwrap().to_string()
             ]
         );
     }
