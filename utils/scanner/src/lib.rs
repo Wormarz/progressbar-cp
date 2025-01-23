79e7d2984a538a36d6b6c7483f1bee0242517cd5
wormarz <wormarz@outlook.com>
2025-01-23 21:59:38 +0800

    Refactor directory scanning functionality into a trait and implement BaseScanner for improved modularity and testability

--- utils/scanner/src/lib.rs
+++ utils/scanner/src/lib.rs
@@ -1,73 +1,18 @@
-use anyhow::Context;
-use log::trace;
-use std::fs;
-use walkdir::WalkDir;
+pub mod scanners;
 
-pub fn scan_dir(srcs: &[String]) -> anyhow::Result<Vec<String>> {
-    let mut new_srcs: Vec<String> = Vec::with_capacity(srcs.len() * 2);
-    for src in srcs {
-        if fs::metadata(src)
-            .with_context(|| format!("failed to read metadata of {}", src))?
-            .is_dir()
-        {
-            for entry in WalkDir::new(src).into_iter().filter_map(Result::ok) {
-                trace!("{}", entry.path().to_str().unwrap());
-                new_srcs.push(entry.path().to_str().unwrap().to_string());
-            }
-        } else {
-            new_srcs.push(src.clone());
+pub trait DirScan {
+    fn in_scan_action(&self, cur_entry: &str) -> anyhow::Result<(Vec<String>, Vec<String>)>;
+
+    fn scan(&self, paths: &[String]) -> anyhow::Result<(Vec<String>, Vec<String>)> {
+        let mut src_paths: Vec<String> = Vec::with_capacity(paths.len() * 2);
+        let mut des_paths: Vec<String> = Vec::with_capacity(paths.len() * 2);
+
+        for path in paths {
+            let (mut src_path, mut des_path) = self.in_scan_action(path)?;
+            src_paths.append(&mut src_path);
+            des_paths.append(&mut des_path);
         }
-    }
-    Ok(new_srcs)
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs::File;
-    use tempfile;
-
-    #[test]
-    fn scan_dir_works() {
-        let temp_dir = tempfile::tempdir_in(".").unwrap();
-        let file_path = temp_dir.path().join("my-temporary-note.txt");
-        let _file = File::create(&file_path).unwrap();
-        let ret = scan_dir(&vec![temp_dir.path().to_str().unwrap().to_string()]).unwrap();
-        println!("{:?}", ret);
-        assert_eq!(
-            ret,
-            vec![
-                temp_dir.path().to_str().unwrap().to_string(),
-                file_path.to_str().unwrap().to_string()
-            ]
-        );
-    }
-
-    #[test]
-    fn scan_stop_at_link() {
-        let temp_dir1 = tempfile::tempdir_in(".").unwrap();
-        let temp_dir2 = tempfile::tempdir_in(".").unwrap();
-
-        let file_path = temp_dir1.path().join("my-temporary-note.txt");
-        let _file = File::create(&file_path).unwrap();
-
-        let link_path = temp_dir2.path().join("my-temporary-note.link");
-        let _link = std::os::unix::fs::symlink(temp_dir1.path(), &link_path).unwrap();
-
-        let ret = scan_dir(&vec![temp_dir2.path().to_str().unwrap().to_string()]).unwrap();
-        println!("{:?}", ret);
-        assert_eq!(
-            ret,
-            vec![
-                temp_dir2.path().to_str().unwrap().to_string(),
-                link_path.to_str().unwrap().to_string()
-            ]
-        );
-    }
-
-    #[test]
-    fn scan_nonexistent_dir() {
-        let ret = scan_dir(&vec!["nonexistent_dir".to_string()]);
-        assert!(ret.is_err());
+        Ok((src_paths, des_paths))
     }
 }
---
+++ utils/scanner/src/scanners/basescanner.rs
@@ -0,0 +1,152 @@
+use super::super::DirScan;
+use anyhow::{Context, Result};
+use log::trace;
+use std::fs;
+use walkdir::WalkDir;
+
+pub struct BaseScanner<'a> {
+    des_path: &'a str,
+}
+
+impl BaseScanner<'_> {
+    pub fn new<'a>(des_path: &'a str) -> BaseScanner<'a> {
+        BaseScanner { des_path }
+    }
+}
+
+impl DirScan for BaseScanner<'_> {
+    fn in_scan_action(&self, cur_entry: &str) -> Result<(Vec<String>, Vec<String>)> {
+        let mut src_paths: Vec<String> = Vec::new();
+        let mut des_paths: Vec<String> = Vec::new();
+
+        let parent_entry = cur_entry
+            .trim_end_matches('/')
+            .trim_end_matches(cur_entry.trim_end_matches('/').rsplit('/').next().unwrap());
+
+        let metadata = fs::metadata(cur_entry)
+            .with_context(|| format!("failed to read metadata of {}", cur_entry))?;
+        if metadata.is_dir() {
+            for entry in WalkDir::new(cur_entry).into_iter().filter_map(Result::ok) {
+                let entry_path = entry.path().to_str().unwrap();
+                trace!("{} found!", entry_path);
+
+                src_paths.push(entry_path.to_string());
+                des_paths.push(self.des_path.to_string() + &entry_path.replace(parent_entry, "/"));
+            }
+        } else {
+            src_paths.push(cur_entry.to_string());
+            des_paths.push(self.des_path.to_string() + &cur_entry.replace(parent_entry, "/"));
+        }
+
+        Ok((src_paths, des_paths))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{fs::File, vec};
+    use tempfile;
+
+    #[test]
+    fn scan_dir_works() {
+        let src_dir = tempfile::tempdir_in(".").unwrap();
+        let src_dir_path = src_dir.path();
+        let src_file_path = src_dir_path.join("my-temporary-note.txt");
+        let _file = File::create(&src_file_path).unwrap();
+
+        let des_dir = tempfile::tempdir_in(".").unwrap();
+        let des_dir_path = des_dir.path();
+
+        let scanner = BaseScanner::new(des_dir_path.to_str().unwrap());
+
+        let (src_paths, des_paths) = scanner
+            .scan(&vec![src_dir_path.to_str().unwrap().to_string()])
+            .unwrap();
+
+        println!("src_paths: {:?}\ndes_paths: {:?}", src_paths, des_paths);
+
+        let sfolder = src_dir_path.to_str().unwrap().to_string();
+        let sfile = src_file_path.to_str().unwrap().to_string();
+        assert_eq!(src_paths, vec![sfolder, sfile]);
+
+        let dfolder = des_dir_path.to_str().unwrap().to_string()
+            + "/"
+            + src_dir_path.to_str().unwrap().rsplit('/').next().unwrap();
+        let dfile =
+            dfolder.clone() + "/" + src_file_path.to_str().unwrap().rsplit('/').next().unwrap();
+
+        assert_eq!(des_paths, vec![dfolder, dfile]);
+    }
+
+    #[test]
+    fn scan_file_works() {
+        let src_dir = tempfile::tempdir_in(".").unwrap();
+        let src_dir_path = src_dir.path();
+        let src_file_path = src_dir_path.join("my-temporary-note.txt");
+        let _file = File::create(&src_file_path).unwrap();
+
+        let des_dir = tempfile::tempdir_in(".").unwrap();
+        let des_dir_path = des_dir.path();
+
+        let scanner = BaseScanner::new(des_dir_path.to_str().unwrap());
+
+        let (src_paths, des_paths) = scanner
+            .scan(&vec![src_file_path.to_str().unwrap().to_string()])
+            .unwrap();
+
+        println!("src_paths: {:?}\ndes_paths: {:?}", src_paths, des_paths);
+
+        let sfile = src_file_path.to_str().unwrap().to_string();
+        assert_eq!(src_paths, vec![sfile]);
+
+        let dfile = des_dir_path.to_str().unwrap().to_string()
+            + "/"
+            + src_file_path.to_str().unwrap().rsplit('/').next().unwrap();
+        assert_eq!(des_paths, vec![dfile]);
+    }
+
+    #[test]
+    fn scan_stop_at_link() {
+        let temp_dir = tempfile::tempdir_in(".").unwrap();
+        let temp_dir_path = temp_dir.path();
+        let file_path = temp_dir_path.join("my-temporary-note.txt");
+        let _file = File::create(&file_path).unwrap();
+
+        let src_dir = tempfile::tempdir_in(".").unwrap();
+        let src_dir_path = src_dir.path();
+        let src_link_path = src_dir_path.join("my-temporary-note.link");
+        let _link = std::os::unix::fs::symlink(temp_dir_path, &src_link_path).unwrap();
+
+        let des_dir = tempfile::tempdir_in(".").unwrap();
+        let des_dir_path = des_dir.path();
+
+        let scanner = BaseScanner::new(des_dir_path.to_str().unwrap());
+        let (src_paths, des_paths) = scanner
+            .scan(&vec![src_dir_path.to_str().unwrap().to_string()])
+            .unwrap();
+        println!("src_paths: {:?}\ndes_paths: {:?}", src_paths, des_paths);
+
+        let sfolder = src_dir_path.to_str().unwrap().to_string();
+        let slink = src_link_path.to_str().unwrap().to_string();
+        assert_eq!(src_paths, vec![sfolder, slink]);
+
+        let dfolder = des_dir_path.to_str().unwrap().to_string()
+            + "/"
+            + src_dir_path.to_str().unwrap().rsplit('/').next().unwrap();
+        let dlink = dfolder.clone()
+            + "/"
+            + src_link_path.to_str().unwrap().rsplit('/').next().unwrap();
+        assert_eq!(des_paths, vec![dfolder, dlink]);
+    }
+
+    #[test]
+    fn scan_nonexistent_dir() {
+        let des_dr = tempfile::tempdir_in(".").unwrap();
+        let des_dir_path = des_dr.path();
+
+        let scanner = BaseScanner::new(des_dir_path.to_str().unwrap());
+        let ret = scanner.scan(&vec!["nonexistent".to_string()]);
+        assert!(ret.is_err());
+    }
+}
