20d45f2e243390d7c61e85f3ecfe6d7d0e3a2821
wormarz <wormarz@outlook.com>
2025-01-25 19:20:33 +0800

    support relative path parsing

--- utils/scanner/src/scanners/basescanner.rs
+++ utils/scanner/src/scanners/basescanner.rs
@@ -22,16 +22,25 @@ impl DirScan for BaseScanner<'_> {
         let parent_entry = cur_entry
             .trim_end_matches('/')
             .trim_end_matches(cur_entry.trim_end_matches('/').rsplit('/').next().unwrap());
+        trace!("parent_entry: {}", parent_entry);
 
         let metadata = fs::metadata(cur_entry)
             .with_context(|| format!("failed to read metadata of {}", cur_entry))?;
         if metadata.is_dir() {
             for entry in WalkDir::new(cur_entry).into_iter().filter_map(Result::ok) {
-                let entry_path = entry.path().to_str().unwrap();
-                trace!("{} found!", entry_path);
+                let src_entry = entry.path().to_str().unwrap();
+                trace!("{} found!", src_entry);
 
-                src_paths.push(entry_path.to_string());
-                des_paths.push(self.des_path.to_string() + &entry_path.replace(parent_entry, "/"));
+                let mut des_entry = if parent_entry == "" {
+                    String::from("/") + src_entry
+                } else {
+                    src_entry.replace(parent_entry, "/").to_string()
+                };
+
+                des_entry.insert_str(0, self.des_path);
+
+                src_paths.push(src_entry.to_string());
+                des_paths.push(des_entry);
             }
         } else {
             src_paths.push(cur_entry.to_string());
@@ -60,15 +69,19 @@ mod tests {
 
         let scanner = BaseScanner::new(des_dir_path.to_str().unwrap());
 
+        let relative_src = src_dir_path.file_name().unwrap().to_string_lossy().to_string();
+
         let (src_paths, des_paths) = scanner
-            .scan(&vec![src_dir_path.to_str().unwrap().to_string()])
+            .scan(&vec![src_dir_path.to_str().unwrap().to_string(), relative_src.clone()])
             .unwrap();
 
         println!("src_paths: {:?}\ndes_paths: {:?}", src_paths, des_paths);
 
         let sfolder = src_dir_path.to_str().unwrap().to_string();
         let sfile = src_file_path.to_str().unwrap().to_string();
-        assert_eq!(src_paths, vec![sfolder, sfile]);
+        let sfolder_relative = relative_src.clone();
+        let sfile_relative = relative_src + "/my-temporary-note.txt";
+        assert_eq!(src_paths, vec![sfolder, sfile, sfolder_relative, sfile_relative]);
 
         let dfolder = des_dir_path.to_str().unwrap().to_string()
             + "/"
@@ -76,7 +89,7 @@ mod tests {
         let dfile =
             dfolder.clone() + "/" + src_file_path.to_str().unwrap().rsplit('/').next().unwrap();
 
-        assert_eq!(des_paths, vec![dfolder, dfile]);
+        assert_eq!(des_paths, vec![dfolder.clone(), dfile.clone(), dfolder, dfile]);
     }
 
     #[test]
